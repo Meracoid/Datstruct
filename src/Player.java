be39794cfbe31c5d72795abc3d62d2b4be29b33c
McNeill Pate <neillpate@gmail.com>
2016-11-28 19:52:16 -0500

    Let player examine gold
    now player can see it has value, instead of not being able to examine it

--- src/Player.java
+++ src/Player.java
@@ -230,6 +230,13 @@ public class Player{
                             System.out.println("Invalid Item in current Room");
                         }
                         break;
+                    case "gold":
+                        if(Arrays.asList(map.getRoomItems(map.getDungeonRoom(this.locationX,this.locationY))).contains("map")) {
+                            System.out.println("precious mineral, can be sold outside of the dungeon for high price");
+                        }else{
+                            System.out.println("Invalid Item in current Room");
+                        }
+                        break;
                     default:
                         System.out.println("Not able to examine");
                 }
